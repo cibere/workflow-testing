e92f8b7c38a7ab279d43cabc428c2e0daf625d09
cibere <71997063+cibere@users.noreply.github.com>
2024-10-27 15:20:03 -0700

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,6 +1,7 @@
 import json
 
-with open("data.json", "w+") as f:
+with open("data.json", "r") as f:
     before = json.load(f)
-    before['release_only'] = True
+before['release_only'] = True
+with open("data.json", "w") as f:
     json.dump(before, f)
