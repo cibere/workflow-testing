fdf6198a50649a715c4fbaa6d620dc8183c512b5
cibere <71997063+cibere@users.noreply.github.com>
2024-10-27 15:45:08 -0700

    d

--- generate.py
+++ generate.py
@@ -3,5 +3,9 @@ import json
 with open("data.json", "r") as f:
     before = json.load(f)
 before['release_only'] = True
+ver = before['Version']
+major, minor, micro = ver.split(".")
+micro = int(micro) + 1
+before['Version'] = f"{major}.{minor}.{micro}"
 with open("data.json", "w") as f:
     json.dump(before, f)
